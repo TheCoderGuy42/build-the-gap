d3081d3a19c53301a79f68b6fd4ef6a7b78d7ff1
Abilash42 <abilash.suresh199@gmail.com>
2025-06-21 16:02:59 -0400

    Update route.ts

--- src/app/api/link/[...all]/route.ts
+++ src/app/api/link/[...all]/route.ts
@@ -1,12 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { all: string[] } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { all: string[] } },
+) {
   // Your logic here
   return NextResponse.json({ message: "Link opened!", params });
-}
-
-export const staticLink = (slug: string) => {
-    const link = `/api/link/${slug}`;
-    console.log("staticLink", slug, link);
-    return link;
 }
