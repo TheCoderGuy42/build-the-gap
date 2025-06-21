85e5ff950ef5bb3961cd4b3f94be9d8c22abef6e
Abilash42 <abilash.suresh199@gmail.com>
2025-06-21 01:29:41 -0400

    Update home-page.tsx

--- src/app/_components/home-page.tsx
+++ src/app/_components/home-page.tsx
@@ -5,5 +5,13 @@ import { useState } from "react";
 import { api } from "~/trpc/react";
 
 export function HomePage() {
-  return <div>hello</div>;
+  // const handlePdf = () => {
+  //   return api.pdfProcess.upload.useQuery();
+  // };
+
+  return (
+    <div className="flex h-50 w-screen items-center justify-center bg-red-400">
+      <input type="file" placeholder="input pdf here" />
+    </div>
+  );
 }
